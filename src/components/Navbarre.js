3f99ff4882a9ffb770ec620f033f4be0e2f824fa
MahaNoureddine <160044003+MahaNoureddine@users.noreply.github.com>
2024-12-17 21:25:49 +0100

    Update Navbarre.js

--- src/components/Navbarre.js
+++ src/components/Navbarre.js
@@ -4,10 +4,15 @@ import { Link, useNavigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import FontAwesomeIcon
 import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons"; // Import basket icon
+import AlertModal from "../components/AlertModal";
+
 
 const Navbarre = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertType, setAlertType] = useState("");
 
   useEffect(() => {
     const auth = getAuth();
@@ -17,7 +22,7 @@ const Navbarre = () => {
     return () => unsubscribe();
   }, []);
 
-  const handleSignOut = async () => {
+  /*const handleSignOut = async () => {
     const auth = getAuth();
     try {
       await signOut(auth);
@@ -27,6 +32,31 @@ const Navbarre = () => {
       console.error("Error signing out:", error);
       alert("Failed to sign out. Please try again.");
     }
+  };*/
+
+  const handleSignOut = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      setAlertMessage("You have successfully signed out.");
+      setAlertType("success");
+      setShowModal(true);
+
+      setTimeout(() => {
+        navigate("/HomePage");
+        setShowModal(false); // Close the modal after redirect
+      }, 1000); // 2-second delay
+
+    } catch (error) {
+      console.error("Error signing out:", error);
+      setAlertMessage("Failed to sign out. Please try again.");
+      setAlertType("error");
+      setShowModal(true);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
 
   const renderUnauthenticatedNavbar = () => (
@@ -57,9 +87,19 @@ const Navbarre = () => {
         <Link to="/Panier" className="navbar-btn">
           <FontAwesomeIcon icon={faShoppingBasket} /> {/* Basket icon */}
         </Link>
-        <div className="auth-buttons">
-          <button onClick={handleSignOut} className="navbar-btn btn btn-link text-decoration-none">Sign Out</button>
-        </div>
+        <Link to="/Avis" className="navbar-btn">Reviews</Link>
+        
+    
+      <button onClick={handleSignOut} className="btn btn-danger">Sign Out</button>
+      {showModal && (
+        <AlertModal
+          message={alertMessage}
+          type={alertType}
+          onClose={handleCloseModal}
+        />
+      )}
+    
+  );
       </div>
     </nav>
   );
